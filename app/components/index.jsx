320501cc1ef0db8acefc0b8f76dcdb703547dc80
Jamieson Abbott <jdabbot@vast-inc.com>
2016-03-03 22:04:18 -0500

    Working on setting up the isomorphic portion

---
+++ app/components/index.jsx
@@ -0,0 +1,7 @@
+
+import { app } from './views/app'
+
+
+render((
+  <app />
+), document.getElementById('react-root'))
--- app/components/views/jsx/app.jsx
+++
@@ -1,7 +0,0 @@
-var React = require('react'),
-    ReactDOM = require('react-dom'),
-    Home = require('./pages/home');
-
- ReactDOM.render(<Home />, document.getElementById('root'));
-
-
