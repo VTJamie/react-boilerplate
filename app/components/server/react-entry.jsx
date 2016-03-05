2a3cf8ad0e57a1a720cda778d163805c2795d1e3
Jamieson Abbott <jdabbott@vt.edu>
2016-03-05 18:06:40 -0500

    Broken partial version of including react router

--- app/components/server/react-entry.jsx
+++ app/components/server/react-entry.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import App  from '../isomorphic/views/App'
+import AppRouter  from '../isomorphic/views/AppRouter'
 
 export default (expressapp) => {
     expressapp.get('/', function(req, res) {
-        res.render('index', {react: renderToString(<App />), livereload: "<script>document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js\"></' + 'script>')</script>"});
+        res.render('index', {react: renderToString(<AppRouter />), livereload: "<script>document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js\"></' + 'script>')</script>"});
     });
 }
